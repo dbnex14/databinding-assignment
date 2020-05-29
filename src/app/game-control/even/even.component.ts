d6590ef01048347be8d8d363e564cc78390bb7be
dbnex <dbnex14@gmail.com>
2020-05-29 12:17:21 -0700

    Solved assignment

--- src/app/game-control/even/even.component.ts
+++ src/app/game-control/even/even.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-even',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./even.component.css']
 })
 export class EvenComponent implements OnInit {
+  // This is number we are passing in from outside into this component, so
+  // we must decorate it with @Input() so we can pass data in.
+  @Input() number: number;
 
   constructor() { }
 
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,17 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  // Decorate event emitter with @Output so it can be cought outside of 
+  // this component.  We can then listen to this event now from other
+  // components using this component.  See app.component.html.
+  // Also, this event emitter expects to receive a number which we can 
+  // pass using $event.
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onStart() {
-    console.log("started game");
+  // when starting game, we should emit an event
+  onStartGame() {
+    this.interval = setInterval(() => 
+    { 
+      // here we want to emit intervalFired event and increment
+      this.intervalFired.emit(++this.lastNumber);
+    }, 1000);
   }
 
-  onStop() {
-    console.log("stopped game");
+  onPauseGame() {
+    clearInterval(this.interval);
   }
 }
